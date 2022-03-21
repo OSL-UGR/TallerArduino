b0396a175872e23ac30847bba05ae2f53101dd76
LocalPinkRobin <robin@maripc.localdomain>
2022-03-21 10:18:16 +0100

    Añadidos pantalla LCD y proyecto final

---
+++ LCD/LCD.ino
@@ -0,0 +1,19 @@
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd(0x27,16,2);
+
+void setup() {
+
+  lcd.begin();
+  lcd.setBacklight(HIGH);
+  lcd.setCursor(0, 0);
+  lcd.print("¡Hola Mundo!");
+  lcd.setCursor(0,1);
+  lcd.print("Hola Mari");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
